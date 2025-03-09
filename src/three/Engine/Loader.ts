33105b81a217c127c253284372a7071778d83bf6
aslmbk <assayf95@gmail.com>
2025-03-09 06:32:39 +0300

    add texture loader

--- src/three/Engine/Loader.ts
+++ src/three/Engine/Loader.ts
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 
@@ -8,15 +9,24 @@ type GLTFLoaderOptions = {
   onError?: (err: unknown) => void;
 };
 
+type TextureLoaderOptions = {
+  url: string;
+  onLoad: (texture: THREE.Texture) => void;
+  onProgress?: (event: ProgressEvent) => void;
+  onError?: (err: unknown) => void;
+};
+
 export class Loader {
   private gltfLoader: GLTFLoader;
+  private textureLoader: THREE.TextureLoader;
 
   constructor() {
     this.gltfLoader = new GLTFLoader();
-
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("/draco/");
     this.gltfLoader.setDRACOLoader(dracoLoader);
+
+    this.textureLoader = new THREE.TextureLoader();
   }
 
   public loadGLTF(options: GLTFLoaderOptions) {
@@ -32,5 +42,20 @@ export class Loader {
     options: Pick<GLTFLoaderOptions, "url" | "onProgress">
   ) {
     return this.gltfLoader.loadAsync(options.url, options.onProgress);
+  }
+
+  public loadTexture(options: TextureLoaderOptions) {
+    return this.textureLoader.load(
+      options.url,
+      options.onLoad,
+      options.onProgress,
+      options.onError
+    );
+  }
+
+  public loadTextureAsync(
+    options: Pick<TextureLoaderOptions, "url" | "onProgress">
+  ) {
+    return this.textureLoader.loadAsync(options.url, options.onProgress);
   }
 }
